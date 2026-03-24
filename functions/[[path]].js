16bdc98496819a93a5b2e84dd1130f33d961f0b3
nianzhe666 <nianzhe666@163.com>
2026-03-24 14:15:13 +0900

    2修改bug

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -4,12 +4,26 @@ export default {
     const url = new URL(request.url);
     const path = url.pathname;
     
+    // 对于静态文件，直接返回
+    if (path.startsWith('/static/') || path.includes('.')) {
+      return env.ASSETS.fetch(request);
+    }
+    
     // 对于根路径，返回index.html
     if (path === '/') {
       return env.ASSETS.fetch(new Request(`${url.origin}/index.html`, request));
     }
     
-    // 对于其他路径，直接返回对应的文件
-    return env.ASSETS.fetch(request);
+    // 对于其他路径，尝试返回对应的HTML文件
+    const htmlPath = `${path}.html`;
+    const htmlRequest = new Request(`${url.origin}${htmlPath}`, request);
+    const htmlResponse = await env.ASSETS.fetch(htmlRequest);
+    
+    if (htmlResponse.status === 200) {
+      return htmlResponse;
+    }
+    
+    // 如果没有对应的HTML文件，返回index.html
+    return env.ASSETS.fetch(new Request(`${url.origin}/index.html`, request));
   },
 };
